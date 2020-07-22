06a78f4308ebad5e42997ee519864aa0d316c659
grogsden <brian.stempien@protonmail.com>
2020-07-22 09:17:24 -0500

    Oauth Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '284843409284642',
+        clientSecret: 'f50a0700ae5c477e400dd20c9726693a'
+    }
 }
